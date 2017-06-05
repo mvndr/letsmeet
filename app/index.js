b5ec5b32991cb858cf8de96dddc9c98089e621fe
Faizal Vasaya <faizvasya@gmail.com>
2017-06-05 20:01:22 +0530

    Node server serves a static page
    The node server serves a static web page  index.html on '/' request.

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 var express =  require('express');
+var path = require('path');
 var app =  express();
+app.use(express.static(path.join(__dirname,'public')));
 app.get('/', function(req, resp){
     resp.send('Hello World');
 });
